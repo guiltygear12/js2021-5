6f0f1d4235078ddabba2ea1c011dac0e9bde4d8c
park <mylove700757@gmail.com>
2021-03-25 22:18:04 +0900

    README 03/23 수업내용 2.0.1

--- hello.js
+++ hello.js
@@ -2,3 +2,22 @@
 console.log("hello world...!");
 console.log("hello world...!");
 console.log("hello world...!");
+
+
+let date = new Date();
+h = date.getHours();
+
+let input = 31;
+
+
+if (date.getHours() < 12){
+    console.log("오전 입니다.")
+} else console.log("오후 입니다.")
+
+
+console.log(date.getFullYear());
+console.log(date.getMonth());
+console.log(date.getDay());  //요일, 날짜가 아님 0=일요일 1=월요일 2= 화요일  
+console.log(date.getHours());
+console.log(date.getMinutes());
+console.log(date.getSeconds());
