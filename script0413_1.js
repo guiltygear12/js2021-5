3f096224d27c7960d0aec93d6633faf54a560a93
park <mylove700757@gmail.com>
2021-04-13 14:56:06 +0900

    수업내용 정리 0413_1교시

---
+++ script0413_1.js
@@ -0,0 +1,21 @@
+let foo = function(){
+    console.log("첫번째 줄");
+    console.log("두번째 줄");
+}
+
+foo();
+console.log(foo);
+
+function a(){
+    console.log("함수의 첫 번째 줄");
+    console.log("함수의 두 번째 줄");
+}
+
+a();
+console.log(a);
+
+let arrow = () => {console.log("화살표 함수");}
+
+
+arrow();
+console.log(arrow);
